c06fed3f1c53839141b58d7cad1d4fa4dbc44fe5
AkshithaReddyManne <92724587+AkshithaReddyManne@users.noreply.github.com>
2021-11-05 20:34:20 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+      <div>
+        <nav className="nav">
+          <div className="nav-left">
+            <a className="brand" href="/">
+              Task Management
+            </a>
+          </div>
+          <div className="nav-right">
+            
+          </div>
+        </nav>
+      </div>
+    );
+  }
+  
+  export default Header;
